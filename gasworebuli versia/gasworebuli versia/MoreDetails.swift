e51e90cdd60ebff6209fc6de0ac7e1414d8463ef
Salo111 <100139026+Salo111@users.noreply.github.com>
2022-08-11 09:24:07 -0700

    Add files via upload

---
+++ gasworebuli versia/gasworebuli versia/MoreDetails.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+class MoreDetails: UIViewController {
+    
+    @IBOutlet weak var popul: UILabel!
+    @IBOutlet weak var capital: UILabel!
+    
+    var country: Country!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    func configure(){
+        popul.text = "\(country.population)"
+       // capital.text = country.capital ამას ლინკზე არ მაჩვენებს და არ ვიცი რატომ
+        
+    }
+
+}
